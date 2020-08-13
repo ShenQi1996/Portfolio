a4f70672e4979b9e59a2ef9fc2fd8663bd3ec194
5232132Abc! <qisam1989@gmail.com>
2020-08-13 14:18:07 -0400

    Need some work on JS

--- Portfolio/portfolio/main.js
+++ Portfolio/portfolio/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     $('.menu-toggler').on('click', function() {
         $(this).toggleClass('open');
@@ -8,22 +10,36 @@ $(document).ready(function () {
             $('.menu-toggle').removeClass('open');
             $('.top-nav').removeClass('open');
         });
+        
     /*  NEED MORE WORK 
     $('.nav a[href*="#"]').on('click', function() {
-            $('html, body').animate( Keyframes:{
+        
+            $('html, body').animate({
                 scrollTop: $($(this).attr('href')).offset().top - 100
-            }, options: 2000);
+            },  2000);
+        
         });
-    
-   $('#up').on('click', function() {
+    */
+        /*NEED MORE WORK*/
+    $(".nav-list a").on("click", function(){
+        var pageId = $(this).attr("data-page");
+        $("html, body").animate({
+            scrollTop: $("#"+pageId).offest().top }, 2000);
+    });
+
+    $('#up').on('click', function() {
+       /*
     $('html, body').animate( Keyframes:{
         scrollTop: 0
     }, options: 2000);
-    });
     */
+        document.documentElement.scrollTop = 0;
+    });
+    
    AOS.init({
        easing: 'ease',
        duration: 1000,
        once: true
+       /* change once will change the web effects*/
    });
 });
